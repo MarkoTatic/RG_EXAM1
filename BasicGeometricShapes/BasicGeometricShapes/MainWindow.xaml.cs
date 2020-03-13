06064174327fc07fe8f92294eca0571b75c84daa
MarkoTatic <46170923+MarkoTatic@users.noreply.github.com>
2020-03-13 18:01:10 +0100

    ellipse added

--- BasicGeometricShapes/BasicGeometricShapes/MainWindow.xaml.cs
+++ BasicGeometricShapes/BasicGeometricShapes/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BasicGeometricShapes.ExtandWindows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,62 @@ namespace BasicGeometricShapes
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static string selectedMenuItem = "";
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ActiveCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Elipse.IsChecked)
+            {
+                selectedMenuItem = "Elipse";
+                Point p = Mouse.GetPosition(ActiveCanvas);
+                ElipseWindow elipseWindow = new ElipseWindow(p, ActiveCanvas);
+                elipseWindow.ShowDialog();
+                elipseWindow.Close();
+            }
+            else if (Rectangle.IsChecked)
+            {
+                selectedMenuItem = "Rectangle";
+            }
+            else if (Polygon.IsChecked)
+            {
+                selectedMenuItem = "Polygon";
+            }
+            else if (Image.IsChecked)
+            {
+                selectedMenuItem = "Image";
+            }          
+        }
+
+        private void Elipse_Click(object sender, RoutedEventArgs e)
+        {
+            Rectangle.IsChecked = false;
+            Polygon.IsChecked = false;
+            Image.IsChecked = false;
+        }
+
+        private void Rectangle_Click(object sender, RoutedEventArgs e)
+        {
+            Elipse.IsChecked = false;
+            Polygon.IsChecked = false;
+            Image.IsChecked = false;
+        }
+
+        private void Polygon_Click(object sender, RoutedEventArgs e)
+        {
+            Rectangle.IsChecked = false;
+            Elipse.IsChecked = false;
+            Image.IsChecked = false;
+        }
+
+        private void Image_Click(object sender, RoutedEventArgs e)
+        {
+            Rectangle.IsChecked = false;
+            Polygon.IsChecked = false;
+            Elipse.IsChecked = false;
         }
     }
 }
